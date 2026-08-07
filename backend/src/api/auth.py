f1c9592dd0f8e0e8e2db794f83dcbf6ddf92ff96
Gyubeom Park <qja0707@gmail.com>
2026-08-07 10:02:33 +0900

    chore: api 실패 시 raise HTTPException

--- backend/src/api/auth.py
+++ backend/src/api/auth.py
@@ -2,17 +2,25 @@ from fastapi import APIRouter
 from src.service.auth import signin_user
 from fastapi import HTTPException
 from src.schemas.auth import SigninRequest, SigninResponse, RefreshRequest
+from src.schemas.common import ErrorResponse
 from src.db_session.db import get_db
 from fastapi import Depends
 
 router = APIRouter(prefix="/auth", tags=["유저 로그인 및 토큰 재발급등 인증 관련"])
 
-@router.post("/signin", response_model=SigninResponse)  
+@router.post("/signin", 
+             response_model=SigninResponse,
+             responses={
+                 401: {
+                     "model": ErrorResponse,
+                     "description": "로그인 실패"
+                 }
+             })  
 def signin(payload: SigninRequest, db=Depends(get_db)):
     token = signin_user(payload.id, payload.pw, db)
 
     if not token:
-        raise HTTPException(status_code=401, detail="로그인 실패")
+        raise HTTPException(status_code=401, detail="아이디 혹은 비밀번호가 일치하지 않습니다")
     
     return token
 
--- backend/src/schemas/common.py
+++ backend/src/schemas/common.py
@@ -1,4 +1,7 @@
 from pydantic import BaseModel
 
+class ErrorResponse(BaseModel):
+    detail: str
+
 class CommonResponse(BaseModel):
     message:str
