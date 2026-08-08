e3f48e326eea1ab8f509bc64912900bc28f08758
Gyubeom Park <qja0707@gmail.com>
2026-08-08 17:19:47 +0900

    feat: 토큰 리프레시 로직 추가

--- backend/src/api/auth.py
+++ backend/src/api/auth.py
@@ -1,9 +1,10 @@
+import jwt
 from fastapi import APIRouter, Depends, HTTPException, status
 
 from src.db_session.db import get_db
-from src.schemas.auth import SigninRequest, SigninResponse
+from src.schemas.auth import RefreshRequest, SigninRequest, SigninResponse
 from src.schemas.common import ErrorResponse
-from src.service.auth import signin_user
+from src.service.auth import signin_user, token_refresh_with_verify
 
 router = APIRouter(prefix="/auth", tags=["유저 로그인 및 토큰 재발급등 인증 관련"])
 
@@ -28,3 +29,24 @@ def signin(payload: SigninRequest, db=Depends(get_db)):
         )
 
     return token
+
+
+@router.post(
+    "/token-refresh",
+    response_model=SigninResponse,
+    responses={
+        status.HTTP_401_UNAUTHORIZED: {
+            "model": ErrorResponse,
+            "description": "잘못된 접근입니다.",
+        }
+    },
+)
+def token_refresh(payload: RefreshRequest, db=Depends(get_db)):
+    try:
+        response = token_refresh_with_verify(payload.refresh_token, db)
+    except jwt.PyJWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="잘못된 접근입니다."
+        )
+
+    return response
